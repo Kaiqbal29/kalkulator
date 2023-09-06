bbfbdd83b4cfcac97499574c4a827014e9ec9761
Iqbal Faris Ihsani <127827865+Kaiqbal29@users.noreply.github.com>
2023-09-06 20:22:12 +0700

    Add files via upload

---
+++ consoleee/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pertemuan1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Aplikasi Calculator Versi Console";
+
+            Console.Write("inputkan nilai a = ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("inputkan nilai b = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, penambahan(a, b));
+            Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, pengurangan(a, b));
+            Console.WriteLine("Hasil Perkalian: {0} x {1} = {2}", a, b, perkalian(a, b));
+            Console.WriteLine("Hasil Pembagian: {0} / {1} = {2}", a, b, pembagian(a, b));
+
+            Console.ReadKey();
+        }
+        static int penambahan(int a, int b)
+        {
+            return a + b;
+        }
+        static int pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        static int perkalian(int a, int b)
+        {
+            return a * b;
+        }
+        static int pembagian(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
